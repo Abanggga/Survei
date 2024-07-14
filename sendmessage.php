ba7a2b65a24d1dae5a56894f7d938415b3bbd53d
Sabilah Amiruddin <142006590+Abanggga@users.noreply.github.com>
2024-07-14 18:35:18 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -1,16 +1,16 @@
 <?php
     $content = '';
     foreach ($_POST as $key => $value) {
-        is($value){
+        if($value){
             $content .= "<b>$key/b>: <i>$value</i>\n";
         }
     }
     if(trim($content)){
-        $content = "<b>Message form Site:<b/b>\n".$content;
+        $content = "<b>Message form Site:</b>\n".$content;
         $apiToken = "6725841625:AAEvPLKn6lpB0y6bsjaITMdmb1k2yLqwCEo";
         $data = [
             'chat_id' => '@Onlysend_messagebots',
-            'text' +> $content,
+            'text' => $content,
             'parse_mode' => 'HTML'
         ];
         $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMassage?".http_build_query($data));
